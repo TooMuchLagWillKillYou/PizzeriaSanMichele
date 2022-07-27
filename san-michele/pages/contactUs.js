e42cec50d395e68add754540277b3a37aa5e474b
Diego Barbaresco <diego.bar@live.it>
2022-07-27 10:25:14 +0000

    example pages

---
+++ san-michele/pages/contactUs.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+import Layout from '../components/layout';
+
+export default function ContactUs() {
+    return (
+      <Layout>
+        <Head>
+          <title>Contatti</title>
+        </Head>
+        <h1 className="text-3xl underline">First Post</h1>
+      </Layout>
+    );
+  }
