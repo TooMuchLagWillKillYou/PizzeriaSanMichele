3904bd903dfe004e20036d8c0e7cd7067ffb8006
Diego Barbaresco <diego.bar@live.it>
2022-07-30 13:44:37 +0000

    provided theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,10 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { ThemeProvider } from "@mui/material";
+import { lightTheme } from "../styles/theme/lightTheme";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <Component {...pageProps} />
+    </ThemeProvider>)
 }
