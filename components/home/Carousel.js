be5a4dfe6f944e34a628cf175bb63c028d94eebe
Diego Barbaresco <diego.bar@live.it>
2022-08-04 20:31:29 +0000

    carousel with test images

--- components/home/Carousel.js
+++ components/home/Carousel.js
@@ -1,16 +1,35 @@
 import React from 'react'
 import useEmblaCarousel from 'embla-carousel-react'
 import Autoplay from 'embla-carousel-autoplay'
+import {images} from "../../images/index"
+import Image from 'next/image'
 
 export const Carousel = () => {
-  const [emblaRef] = useEmblaCarousel({ loop: true, align: "start", draggable: false, speed: 2 }, [Autoplay()])
-
+  console.log("images", images[1]);
+  const [emblaRef] = useEmblaCarousel({ loop: true, align: "start", draggable: false, speed: 5 }, [Autoplay({ delay: 3000 })])
+  const slides = Array.from(Array(3).keys());
+  
   return (
-    <div className="embla" ref={emblaRef}>
-      <div className="embla__container">
-        <div className="embla__slide"><div style={{ height: "100px", width: "100px", backgroundColor: "red" }}/></div>
-        <div className="embla__slide"><div style={{ height: "100px", width: "100px", backgroundColor: "blue" }}/></div>
-        <div className="embla__slide"><div style={{ height: "100px", width: "100px", backgroundColor: "yellow" }}/></div>
+    <div className="embla">
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla__container">
+          {
+            slides.map((index) => (
+              <div className="embla__slide" key={index}>
+                <div className="embla__slide__inner">
+                  <Image
+                    src={images[index].src}
+                    blurDataURL={images[index].blurDataURL}
+                    width={images[index].width}
+                    height={images[index].height}
+                    alt="cover image"
+                    className="embla__slide__img"
+                  />
+                </div>
+              </div>
+            ))
+          }
+        </div>
       </div>
     </div>
   )
