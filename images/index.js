b5f713cbeadaa5ef94f45d90e69b7f1d44023981
Diego Barbaresco <diego.bar@live.it>
2022-08-06 10:08:47 +0000

    definitive images

--- images/index.js
+++ images/index.js
@@ -1,5 +1,6 @@
-import pic01 from "./F01.jpg";
-import pic02 from "./F02.jpg";
-import pic03 from "./F03.jpg";
+import internoPizzeria01 from "./interno-pizzeria-01.jpg";
+import internoPizzeria02 from "./interno-pizzeria-02.jpg";
+import internoPizzeria03 from "./interno-pizzeria-03.jpg";
+import esternoPizzeria01 from "./esterno-pizzeria-01.jpg";
 
-export const images = [pic01, pic02, pic03];
+export const images = [internoPizzeria01, internoPizzeria02, internoPizzeria03, esternoPizzeria01];
