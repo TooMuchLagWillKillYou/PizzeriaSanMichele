0297a104ebb31d19c0fd4b9cbe5c16fef8f99ef5
Diego Barbaresco <diego.bar@live.it>
2022-08-24 19:39:28 +0000

    run prettier

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,21 @@
-import '../styles/globals.css';
+import "../styles/globals.css";
 import { ThemeProvider } from "@mui/material";
 import { lightTheme } from "../styles/theme/lightTheme";
-import createEmotionCache from '../utils/createEmotionCache';
-import { CacheProvider } from '@emotion/react';
+import createEmotionCache from "../utils/createEmotionCache";
+import { CacheProvider } from "@emotion/react";
 
-const clientSideEmotionCache = createEmotionCache()
+const clientSideEmotionCache = createEmotionCache();
 
-export default function MyApp({ Component, emotionCache = clientSideEmotionCache, pageProps }) {
+export default function MyApp({
+  Component,
+  emotionCache = clientSideEmotionCache,
+  pageProps,
+}) {
   return (
     <CacheProvider value={emotionCache}>
       <ThemeProvider theme={lightTheme}>
         <Component {...pageProps} />
       </ThemeProvider>
     </CacheProvider>
-  )
+  );
 }
--- pages/contactUs.js
+++ pages/contactUs.js
@@ -1,13 +1,13 @@
-import Head from 'next/head';
-import Layout from '../components/layout';
+import Head from "next/head";
+import Layout from "../components/layout";
 
 export default function ContactUs() {
-    return (
-      <Layout>
-        <Head>
-          <title>Contatti</title>
-        </Head>
-        <h1 className="text-3xl underline">First Post</h1>
-      </Layout>
-    );
-  }
+  return (
+    <Layout>
+      <Head>
+        <title>Contatti</title>
+      </Head>
+      <h1 className="text-3xl underline">First Post</h1>
+    </Layout>
+  );
+}
